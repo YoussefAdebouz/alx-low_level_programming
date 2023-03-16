4f7afe9813b89d08ffd0636c22cedca2bf656572
youssefadbz <youssef.adebouz@gmail.com>
2023-03-16 12:29:37 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The purpose of this file is to output
+ * followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
