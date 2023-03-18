15c8906202fde4d1979958a1c7ab36552170e7dd
youssefadbz <youssef.adebouz@gmail.com>
2023-03-17 17:57:02 -0700

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - This program prints the numbers from 00 to 99.
- * Numbers are separated by a comma and a space, are printed
- * in ascending order, and putchar is used to print to screen.
- * Return: Always 0 (Success)
+ * main - Print the numbers of 000 to 999.
+ * Return: 0
  */
 
 int main(void)
 {
     int num1, num2, num3;
+
     for (num1 = '0'; num1 <= '9'; num1++)
     {
         num2 = num1 + 1;
+
         for (; num2 <= '9'; num2++)
         {
             num3 = num2 + 1;
+
             for (; num3 <= '9'; num3++)
             {
                 putchar(num1);
                 putchar(num2);
                 putchar(num3);
+
                 if (num1 == '7' && num2 == '8' && num3 == '9')
                 {
                     break;
                 }
+
                 putchar(',');
                 putchar(' ');
             }
         }
     }
-    putchar(10);
+
+    putchar('\n');
     return (0);
 }
-
