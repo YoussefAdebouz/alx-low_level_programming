61a89aa3e817ca7d5ae80fa7fa357710dfe4da5c
youssefadbz <youssef.adebouz@gmail.com>
2023-03-23 07:20:26 -0700

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie two integers
+ * @a: Number 1
+ * @b: Number 2
+ * Return: The result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
