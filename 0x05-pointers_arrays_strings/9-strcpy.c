64d9cf0d4fd7a3507d8094d535fa303703c5548c
youssefadbz <youssef.adebouz@gmail.com>
2023-03-27 08:02:53 -0700

    a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string in src to dest
+ *
+ * @dest: destiny char
+ * @src: source char
+ * Return: copy char in destiny
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+		dest[index] = src[index];
+	dest[index] = '\0';
+	return (dest);
+}
