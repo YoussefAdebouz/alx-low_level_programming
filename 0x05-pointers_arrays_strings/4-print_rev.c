96f16d6a745630fc3198ba1445745193af6a79ff
youssefadbz <youssef.adebouz@gmail.com>
2023-03-27 07:38:21 -0700

    a function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse.
+ * @s: Pointer to string
+ */
+
+void print_rev(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		;
+
+	for (size--; size >= 0; size--)
+	{
+		_putchar(s[size]);
+	}
+
+	_putchar(10);
+}
