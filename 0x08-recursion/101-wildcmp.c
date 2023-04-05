ae183b6b928a1cbf7123f7029717d396e0c85598
youssefadbz <youssef.adebouz@gmail.com>
2023-04-05 08:54:24 -0700

    Readme

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,56 +1,28 @@
 #include "main.h"
-
 /**
- * strlen_recursion - This function finds the length of a string
- * @s: The string to evaluate
- * Return: The length of the string s
+ * wildcmp - Main Entry
+ * @s1: input
+ * @s2: input
+ * Return: 0
  */
-
-int strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	/* if string is empty, return 1 as palindrome */
-	if (*s == '\0')
+	if (*s1 == '\0')
 	{
-		return (0);
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
-	/* call function to return length */
-	s++;
-	return (1 + strlen_recursion(s));
-}
 
-/**
- * helper - This function evaluates a string
- * @s: The string to evaluate
- * @n: The length of the string
- * Return: 1 if s is a palindrom, otherwise 0
- */
-
-int helper(char *s, int len)
-{
-	/* if string is empty, return 1 as palindrome */
-	if (len <= 1)
-		return (1);
-	/* if current index is equal to half the index */
-	else if (*s == *(s + len - 1))
-	    return (helper(s + 1, len - 2));
-	else
-		return (0);
-}
-
-/**
- * is_palindrome - This function evaluates whether a string is a palindrome
- * @s: The string to evaluate
- * Return: 1 if s is a palindrome, otherwise 0
- */
-
-int is_palindrome(char *s)
-{
-	int len;
-
-	len = strlen_recursion(s);
-
-	if (len <= 1)
-		return (1);
-
-	return(helper(s, len));
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
