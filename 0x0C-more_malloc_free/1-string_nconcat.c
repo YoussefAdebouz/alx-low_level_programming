fcec6a6556dbc58e0406c38ddeb1181c80df4399
youssefadbz <youssef.adebouz@gmail.com>
2023-04-13 08:20:24 -0700

    a function that concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - Count the length of a string.
+ * @s: String.
+ * Return: Length.
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int c;
+
+	for (c = 0; s[c]; c++)
+		;
+
+	return (c);
+}
+
+/**
+ * string_nconcat - concatenates two strings untill the n first letters
+ * of string 2.
+ * @s1: String 1.
+ * @s2: String 2.
+ * @n: Number of characters of string 2.
+ * Return: Pointer shall point to a newly allocated space in memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *strcatn = NULL;
+	unsigned int size1, size2, i, j;
+
+	/*Convert null strings to empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	/*Allocate memory*/
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	if (n > size2)
+		n = size2;
+
+	strcatn = malloc(sizeof(char) * (size1 + n + 1));
+
+	if (strcatn == NULL)
+		return (NULL);
+
+	/*Concatenate*/
+	for (i = 0; s1[i] != '\0'; i++)
+		strcatn[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		strcatn[i + j] = s2[j];
+
+	strcatn[i + j] = '\0';
+
+	return (strcatn);
+}
