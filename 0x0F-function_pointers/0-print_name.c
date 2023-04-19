2c3f1f01d09444b7f3990192368316ef99ac2b7a
youssefadbz <youssef.adebouz@gmail.com>
2023-04-19 08:21:57 -0700

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - Print a name.
- * @name: String with a name.
- * @f: Funtion pointer.
- * Return: void.
- */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-	{
-		f(name);
-	}
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
 #include <stdlib.h>
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+
+#endif
