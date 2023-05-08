9df466d6f770c1dca638fe7e354237c37009484a
youssefadbz <youssef.adebouz@gmail.com>
2023-05-08 07:43:09 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,64 +1,46 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: pointer to the name of the file to create.
- * @text_content: pointer to the null terminated string to write to file.
- *
- * fails when file cannot be created, file cannot be written etc.
- * The created file must have those permissions: rw-------. If the file,
- * already exists, do not change the permissions.
- * if the file already exists, truncate it.
- * if filename is NULL return -1.
- * if text_content is NULL create an empty file.
- *
- * Return: 1 on success, -1 on failure.
+ * _strlen - Count the length of a string.
+ * @s: String.
+ * Return: Length.
  */
-
-int create_file(const char *filename, char *text_content)
+int _strlen(char *s)
 {
-	int file_descriptor;
-	int wwrite;
+	unsigned int c;
 
-	/* if filename is NULL */
-	if (!filename)
-		return (-1);
+	for (c = 0; s[c]; c++)
+		;
 
-	/* if text_content is NULL create an empty file */
-	if (!text_content)
-		text_content = "";
-
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	/* If opening fails */
-	if (file_descriptor == -1)
-		return (-1);
-
-	/* write into file */
-	wwrite = write(file_descriptor, text_content, _strlen(text_content));
-	/* if write fails */
-	if (wwrite == -1)
-		return (-1);
-
-	/* close file */
-	close(file_descriptor);
-
-	return (1);
+	return (c);
 }
 
 /**
- * _strlen - Function that returns the length of string.
- * @s: pointer to the null-terminated byte string to be examined.
- *
- * Return: The length of the null-terminated byte string s.
+ * create_file - Create a file.
+ * @filename: File name.
+ * @text_content: Text content to write.
+ * Return: 1 on success, -1 on failure.
  */
-
-int _strlen(const char *s)
+int create_file(const char *filename, char *text_content)
 {
-	int len = 0;
+	int fd;
+	int n_chars;
 
-	while (s[len] != '\0')
-		len++;
-	return (len);
+	if (filename == NULL)
+		return (-1);
+
+	/*Create file*/
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+
+	/*Write*/
+
+	if (text_content != NULL)
+	{
+		n_chars = _strlen(text_content);
+		write(fd, text_content, n_chars);
+	}
+	close(fd);
+	return (1);
 }
