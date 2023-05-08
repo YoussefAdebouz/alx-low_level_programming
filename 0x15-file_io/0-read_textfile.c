613c30ca0936ef3ae1eb63454f2f3b0304f386ad
youssefadbz <youssef.adebouz@gmail.com>
2023-05-08 07:37:03 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,49 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * read_textfile - Read a text file and prints it to the POSIX standard output.
- * @filename: File name.
- * @letters:The number of letters it should read and print.
- * Return: The actual number of letters it could read and print.
+ * read_textfile - function that reads a txt file and prints it to,
+ * POSIX standard out.
+ * @filename: pointer to the file.
+ * @letters: number of letters to read and print.
+ *
+ * If file cannot be opened or read return 0.
+ * if filename is NULL return 0.
+ * if write fails or doesn't write expected amount of bytes return 0.
+ *
+ * Return: actual number of letters it could read and print.
+ *
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;/*File descriptor*/
-	ssize_t n_chars;
-	char *buf = NULL;
+	int file_descriptor;
+	ssize_t wwrite, sz;
+	char *buf; /* pointer to buffer to read data from */
 
-	if (filename == NULL)
+	/* if filename is NULL */
+	if (!filename)
 		return (0);
 
-	/*Read file*/
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	/* opening the file */
+	file_descriptor = open(filename, O_RDONLY);
+	/* If opening fails */
+	if (file_descriptor == -1)
 		return (0);
 
-	/*Allocate memory*/
-	buf = malloc(letters * sizeof(char));
-	if (buf == NULL)
+	buf = malloc(sizeof(char) * letters);
+	/* if no buffer */
+	if (!buf)
 		return (0);
+	/* read the file */
+	sz = read(file_descriptor, buf, letters);
+	buf[sz] = '\0';
 
-	n_chars = read(fd, buf, letters);
-
-	if (n_chars < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
-	n_chars = write(STDOUT_FILENO, buf, n_chars);
-
-	if (n_chars < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
+	/* write to stdout */
+	wwrite = write(STDOUT_FILENO, buf, sz);
+	/* close file */
+	close(file_descriptor);
+	/* free buf */
 	free(buf);
-	close(fd);
-
-	return (n_chars);
+	/* return number of letters */
+	return (wwrite);
 }
